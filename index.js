b1e36731802f4ca29cfebe06c25d50d8a24502df
max bischof <max@bischof.dk>
2020-07-22 14:44:38 +0200

    Use defer in html js script load and remove Dom read listener in js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 "use strict";
 
-var toggleAnswer = function toggleAnswer() {
-  var button = document.querySelector('.answer-button');
-  var answer = document.querySelector('.answer-text');
-  button.addEventListener('click', function () {
-    answer.classList.remove('display-none');
-    button.classList.add('display-none');
-    console.log('click');
-  });
-};
-
-document.addEventListener("DOMContentLoaded", toggleAnswer);
+var button = document.querySelector('.answer-button');
+var answer = document.querySelector('.answer-text');
+button.addEventListener('click', function () {
+  answer.classList.remove('display-none');
+  button.classList.add('display-none');
+  console.log('click');
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-const toggleAnswer = () => {
-  const button = document.querySelector('.answer-button')
-  const answer = document.querySelector('.answer-text')
+const button = document.querySelector('.answer-button')
+const answer = document.querySelector('.answer-text')
 
 button.addEventListener('click', () => {
   answer.classList.remove('display-none')
   button.classList.add('display-none')
   console.log('click')
 })
-}
-
-document.addEventListener("DOMContentLoaded", toggleAnswer);
-
