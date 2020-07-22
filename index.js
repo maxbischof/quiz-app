13f85afd93b17d72c906dc07f866e0701990bae8
max bischof <max@bischof.dk>
2020-07-22 15:42:50 +0200

    Add js for javascript navigation of page-divs

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ buttonAnswer.addEventListener('click', function () {
   buttonAnswer.classList.add('display-none');
   console.log('click');
 });
+var buttonQuestions = document.querySelector('.footer__icon--questions');
+var buttonBookmarks = document.querySelector('.footer__icon--bookmarks');
+var buttonCreate = document.querySelector('.footer__icon--create');
+var buttonProfile = document.querySelector('.footer__icon--profile');
+var pageQuestions = document.querySelector('.page__questions');
+var pageBookmarks = document.querySelector('.page__bookmarks');
+var pageCreate = document.querySelector('.page__create');
+var pageProfile = document.querySelector('.page__profile');
+var currentPage = pageQuestions;
+buttonQuestions.addEventListener('click', function () {
+  pageQuestions.classList.remove('display-none');
+  currentPage.classList.add('display-none');
+  currentPage = pageQuestions;
+});
+buttonBookmarks.addEventListener('click', function () {
+  pageBookmarks.classList.remove('display-none');
+  currentPage.classList.add('display-none');
+  currentPage = pageBookmarks;
+});
+buttonCreate.addEventListener('click', function () {
+  pageCreate.classList.remove('display-none');
+  currentPage.classList.add('display-none');
+  currentPage = pageCreate;
+});
+buttonProfile.addEventListener('click', function () {
+  pageProfile.classList.remove('display-none');
+  currentPage.classList.add('display-none');
+  currentPage = pageProfile;
+});
--- src/index.js
+++ src/index.js
@@ -7,3 +7,37 @@ buttonAnswer.addEventListener('click', () => {
   console.log('click')
 })
 
+const buttonQuestions = document.querySelector('.footer__icon--questions')
+const buttonBookmarks = document.querySelector('.footer__icon--bookmarks')
+const buttonCreate = document.querySelector('.footer__icon--create')
+const buttonProfile = document.querySelector('.footer__icon--profile')
+
+const pageQuestions = document.querySelector('.page__questions')
+const pageBookmarks = document.querySelector('.page__bookmarks')
+const pageCreate = document.querySelector('.page__create')
+const pageProfile = document.querySelector('.page__profile')
+let currentPage = pageQuestions
+
+buttonQuestions.addEventListener('click', () => {
+  pageQuestions.classList.remove('display-none')
+  currentPage.classList.add('display-none')
+  currentPage = pageQuestions
+})
+
+buttonBookmarks.addEventListener('click', () => {
+  pageBookmarks.classList.remove('display-none')
+  currentPage.classList.add('display-none')
+  currentPage = pageBookmarks
+})
+
+buttonCreate.addEventListener('click', () => {
+  pageCreate.classList.remove('display-none')
+  currentPage.classList.add('display-none')
+  currentPage = pageCreate
+})
+
+buttonProfile.addEventListener('click', () => {
+  pageProfile.classList.remove('display-none')
+  currentPage.classList.add('display-none')
+  currentPage = pageProfile
+})
