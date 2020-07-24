d053f3482b97164f640a81d9554ae62609a31703
max bischof <max@bischof.dk>
2020-07-24 11:10:19 +0200

    Split navigation js in file

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,5 @@
 import Card from './card'
-import {get} from './utilities'
+import Nav from './nav'
 
+Nav()
 Card()
-
-const buttonQuestions = get('.footer__icon--questions')
-const buttonBookmarks = get('.footer__icon--bookmarks')
-const buttonCreate = get('.footer__icon--create')
-const buttonProfile = get('.footer__icon--profile')
-
-const pageQuestions = get('.page__questions')
-const pageBookmarks = get('.page__bookmarks')
-const pageCreate = get('.page__create')
-const pageProfile = get('.page__profile')
-let currentPage = pageQuestions
-
-buttonQuestions.addEventListener('click', () => {
-  changePage(pageQuestions)
-  setTitle('Questions')
-})
-
-buttonBookmarks.addEventListener('click', () => {
-  changePage(pageBookmarks)
-  setTitle('Bookmarks')
-})
-
-buttonCreate.addEventListener('click', () => {
-  changePage(pageCreate)
-  setTitle('Create question')
-})
-
-buttonProfile.addEventListener('click', () => {
-  changePage(pageProfile)
-  setTitle('Profile')
-})
-
-function setTitle (title) {
-  document.querySelector('h1').innerHTML = title
-}
-
-function changePage (page) {
-  if (page!=currentPage) {
-    page.classList.remove('display-none')
-    currentPage.classList.add('display-none')
-    currentPage = page
-  }
-}
