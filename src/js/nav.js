5515ee4b71c241099ea899813f768473055106a0
max bischof <max@bischof.dk>
2020-07-27 21:54:15 +0200

    Add getAll function to utils, contraste active footer icon

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,4 @@
-import { get } from './utilities'
+import { get, getAll } from './utilities'
 
 function nav() {
   const buttonQuestions = get('.footer__icon--questions')
@@ -10,31 +10,32 @@ function nav() {
   const pageBookmarks = get('.page__bookmarks')
   const pageCreate = get('.page__create')
   const pageProfile = get('.page__profile')
+
   let currentPage = pageQuestions
 
   buttonQuestions.addEventListener('click', () => {
     changePage(pageQuestions)
     setTitle('Questions')
+    setActiveFooterIcon('footer__icon--questions')
   })
 
   buttonBookmarks.addEventListener('click', () => {
     changePage(pageBookmarks)
     setTitle('Bookmarks')
+    setActiveFooterIcon('footer__icon--bookmarks')
   })
 
   buttonCreate.addEventListener('click', () => {
     changePage(pageCreate)
     setTitle('Create question')
+    setActiveFooterIcon('footer__icon--create')
   })
 
   buttonProfile.addEventListener('click', () => {
     changePage(pageProfile)
     setTitle('Profile')
+    setActiveFooterIcon('footer__icon--profile')
   })
-
-  function setTitle(title) {
-    document.querySelector('h1').innerHTML = title
-  }
 
   function changePage(page) {
     if (page != currentPage) {
@@ -43,6 +44,18 @@ function nav() {
       currentPage = page
     }
   }
+}
+
+function setTitle(title) {
+  document.querySelector('h1').innerHTML = title
+}
+
+function setActiveFooterIcon (activeClass) {
+  const icons = getAll('.footer__icon')
+  icons.forEach(icon => {
+    icon.classList.toggle('footer__icon--active', icon.classList.contains(activeClass))
+    console.log(icon.classList.contains(activeClass))
+  })
 }
 
 export default nav
