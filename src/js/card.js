eca154637f3c13c29e1bdf1efccfccfb659bea4b
max bischof <max@bischof.dk>
2020-07-29 15:17:38 +0200

    Change icon of bookmark button

--- src/js/card.js
+++ src/js/card.js
@@ -1,5 +1,6 @@
 import { get } from './utilities'
 import arrowImg from '../../img/arrow.png'
+import bookmarkButton from '../../img/bookmark.png'
 
 const cards = [
   {
@@ -49,7 +50,13 @@ function addCard(card, target) {
   const unsplashURL = 'https://source.unsplash.com/random' + '/' + Math.floor(Math.random() * 100)
 
   cardSection.innerHTML = /*html*/ `
-    <div data-js='bookmark__button' class="card__bookmark"></div>
+    <svg class="card__bookmark" data-js="bookmark__button" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    viewBox="0 0 512.019 512.019" style="enable-background:new 0 0 512.019 512.019;" xml:space="preserve">
+    <path d="M362.676,0H149.343C119.888,0,96.01,23.878,96.01,53.333v448
+    c-0.011,5.891,4.757,10.675,10.648,10.686c2.84,0.005,5.565-1.123,7.571-3.134L256.01,367.083l141.781,141.781
+    c4.16,4.171,10.914,4.179,15.085,0.019c2.006-2.001,3.133-4.717,3.134-7.55v-448C416.01,23.878,392.131,0,362.676,0z"/>
+    </svg>
+
     <img
       class="card__img"
       src=${unsplashURL}
